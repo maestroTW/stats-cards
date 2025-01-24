2615dcc3dca76b4c877364ef99b75e0b0e8780fe
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-01-24 22:48:28 +0300

    chore: added wrap text fn

---
+++ src/utils/svg.rs
@@ -0,0 +1,21 @@
+const MAX_LINES: usize = 2;
+
+pub fn wrap_text(text: &String) -> String {
+    let lines = textwrap::wrap(text, 60);
+    let wrapped_text: Vec<String> = lines
+        .iter()
+        .enumerate()
+        .filter(|(idx, _)| idx <= &MAX_LINES)
+        .map(|(idx, line)| {
+            let mut text = line.to_string();
+            if idx == MAX_LINES {
+                text += "...";
+            }
+
+            // add escape html
+            format!(r##"<tspan dy="1.25em" x="0">{text}</tspan>"##)
+        })
+        .collect();
+
+    wrapped_text.join("\n")
+}
